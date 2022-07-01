608c5d4a4aeeda8af39e716c36acf053a09fe667
brunobandelli <brunobandelli@gmail.com>
2022-07-01 08:51:40 -0300

    Desenvolvendo componente VIdeo

--- event-plataform/src/components/Sidebar.tsx
+++ event-plataform/src/components/Sidebar.tsx
@@ -1,3 +1,51 @@
+import { gql, useQuery } from "@apollo/client";
+import { Lesson } from "./Lesson";
+
+const GET_LESSON_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`;
+
+interface GetlessonQueryResponse {
+  lessons: {
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    lessonType: "live" | "class";
+  }[];
+}
+
 export function Sidebar() {
-  return <h1>Sidebar</h1>;
+  const { data } = useQuery<GetlessonQueryResponse>(GET_LESSON_QUERY);
+
+  console.log(data);
+
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Conograma de Aulas
+      </span>
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType}
+            />
+          );
+        })}
+      </div>
+    </aside>
+  );
 }
--- event-plataform/src/components/Video.tsx
+++ event-plataform/src/components/Video.tsx
@@ -1,3 +1,70 @@
+import { DiscordLogo, Lightning } from "phosphor-react";
+
 export function Video() {
-  return <h1>Video</h1>;
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+      </div>
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">
+              Aula 01 - Abertura do Ignite Lab
+            </h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              Nessa aula vamos dar início ao projeto criando a estrutura base da
+              aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também
+              realizar o setup do nosso projeto no GraphCMS criando as entidades
+              da aplicação e integrando a API GraphQL gerada pela plataforma no
+              nosso front-end utilizando Apollo Client.
+            </p>
+
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                src="https://github.com/brunobandelli.png"
+                alt=""
+              />
+
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">
+                  Bruno Bandelli
+                </strong>
+                <span className="text-gray-200 text-sm block">
+                  CTO @Bandellibrothers
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <a
+              href=""
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Comunidade do Discord
+            </a>
+            <a
+              href=""
+              className="p-4 text-sm border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
+              <Lightning size={24} />
+              Comunidade do Discord
+            </a>
+          </div>
+        </div>
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div></div>
+            <div></div>
+            <div></div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
